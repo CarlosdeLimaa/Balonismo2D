6ab7a6d085be0ff2f5a0f96a6eb14a476157a3e5
rm86453 <carlosmatheus.0613@gmail.com>
2023-06-15 11:54:18 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criar objetos do passeio
+        Pessoa p1 = new Pessoa();
+        Pessoa p2; //declaração dp objeto
+        p2 = new Pessoa(); // construção
+        //...
+        p1.nome = "Elliot Page";
+
+    }
+}
